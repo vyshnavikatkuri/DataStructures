bffe8fdaa100e143d0cc878d57dd6a2354b70b1d
vyshnavikatkuri <142222557+vyshnavikatkuri@users.noreply.github.com>
2025-08-11 14:24:44 +0530

    Update aggressive cows.cpp

--- Binarysearch/aggressive cows.cpp
+++ Binarysearch/aggressive cows.cpp
@@ -1,4 +1,4 @@
-//{ Driver Code Starts
+Given an array stalls[] which denotes the position of a stall and an integer k which denotes the number of aggressive cows. The task is to assign stalls to k cows such that the minimum distance between any two of them is the maximum possible.//{ Driver Code Starts
 // Initial function template for C++
 
 #include <bits/stdc++.h>
